378a430f61efdc1b2f8eaf94c1d0801136b02655
franchupedu <franchupedu@outlook.com>
2019-11-07 18:03:35 -0300

    TP SUPER POSTAAAAAAgit add .git add .git add .!

---
+++ Tp_Super_Posta/Tp_Super_Posta/Interpolador.Designer.cs
@@ -0,0 +1,312 @@
+﻿namespace Tp_Super_Posta
+{
+    partial class Interpolador
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.Abscisa = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Ordenada = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label16 = new System.Windows.Forms.Label();
+            this.label17 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(12, 9);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(427, 13);
+            this.label10.TabIndex = 0;
+            this.label10.Text = "Ingrese los puntos por los cuales desea que interpole su polinomio en el cuedro d" +
+    "e abajo.";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(12, 22);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(481, 13);
+            this.label11.TabIndex = 1;
+            this.label11.Text = "Luego elija un método de entre los cuales se muestran a la derecha y presione el " +
+    "botón de interpolar.";
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Checked = true;
+            this.radioButton4.Location = new System.Drawing.Point(556, 20);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(72, 17);
+            this.radioButton4.TabIndex = 3;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "LaGrange";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(556, 60);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(155, 17);
+            this.radioButton5.TabIndex = 4;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "Newton-Gregory Progresivo";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            this.radioButton5.CheckedChanged += new System.EventHandler(this.radioButton5_CheckedChanged);
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Location = new System.Drawing.Point(556, 100);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(153, 17);
+            this.radioButton6.TabIndex = 5;
+            this.radioButton6.TabStop = true;
+            this.radioButton6.Text = "Newton-Gregory Regresivo";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            this.radioButton6.CheckedChanged += new System.EventHandler(this.radioButton6_CheckedChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(556, 141);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(155, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Interpolar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(12, 191);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(39, 13);
+            this.label12.TabIndex = 7;
+            this.label12.Text = "Pasos:";
+            this.label12.Click += new System.EventHandler(this.label12_Click);
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(553, 191);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(58, 13);
+            this.label13.TabIndex = 8;
+            this.label13.Text = "Resultado:";
+            this.label13.Click += new System.EventHandler(this.label13_Click);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Abscisa,
+            this.Ordenada});
+            this.dataGridView2.Location = new System.Drawing.Point(15, 38);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView2.TabIndex = 9;
+            this.dataGridView2.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView2_CellContentClick_1);
+            // 
+            // Abscisa
+            // 
+            this.Abscisa.Frozen = true;
+            this.Abscisa.HeaderText = "x";
+            this.Abscisa.Name = "Abscisa";
+            this.Abscisa.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            // 
+            // Ordenada
+            // 
+            this.Ordenada.Frozen = true;
+            this.Ordenada.HeaderText = "f(x)";
+            this.Ordenada.Name = "Ordenada";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(308, 115);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 10;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(308, 89);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 11;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(281, 118);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(21, 13);
+            this.label14.TabIndex = 12;
+            this.label14.Text = "f(x)";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(290, 92);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(12, 13);
+            this.label15.TabIndex = 13;
+            this.label15.Text = "x";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(308, 141);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 23);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "Agregar Punto";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(308, 63);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 15;
+            this.textBox3.Text = "(opcional)";
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Location = new System.Drawing.Point(293, 66);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(9, 13);
+            this.label16.TabIndex = 16;
+            this.label16.Text = "i";
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Location = new System.Drawing.Point(289, 175);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(13, 13);
+            this.label17.TabIndex = 17;
+            this.label17.Text = "k";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(308, 170);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 18;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // Interpolador
+            // 
+            this.ClientSize = new System.Drawing.Size(961, 338);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label17);
+            this.Controls.Add(this.label16);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.radioButton6);
+            this.Controls.Add(this.radioButton5);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Name = "Interpolador";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn x;
+        private System.Windows.Forms.DataGridViewTextBoxColumn y;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Abscisa;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Ordenada;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
---
+++ Tp_Super_Posta/Tp_Super_Posta/Interpolador.cs
@@ -0,0 +1,291 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tp_Super_Posta
+{
+    public partial class Interpolador : Form
+    {
+        List<double> Abscisas = new List<double>();
+        List<double> Ordenadas = new List<double>();
+
+        public Interpolador()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs fila)
+        {
+            int n = fila.RowIndex;
+            if (n != -1)
+            {
+                Abscisas[n] = Convert.ToDouble(dataGridView1.Rows[n].Cells[0].Value);
+                Ordenadas[n] = Convert.ToDouble(dataGridView1.Rows[n].Cells[1].Value);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label12.Text = "Pasos:";
+            if (radioButton1.Checked)
+            {
+                //LaGrange();
+            }
+            else if (radioButton2.Checked)
+            {
+                //NGP();
+            }
+            //else if (radioButton3.Checked)
+            //{
+
+            //}
+        }
+
+        private void LaGrange(double k)
+        {
+            //FALTA HACER QUE LA PERSONA INGRESE LOS PUNTOS POR PANTALLA
+            int cantidadElementos = Abscisas.Count; //CALCULE SOLO
+            double incognita = k; //el valor para verificar el polinomio
+            double resultado = 0;
+            //el polinomio que pusimos de ejemplo es x^2 + x + 1
+            //int[] lx = new int[cantidadElementos];//no sirve
+            int i = 0;
+
+
+            while (i < cantidadElementos)
+            {
+                int j = 0;
+                double L = 1;
+                while (j < cantidadElementos)
+                {
+                    if (i != j)
+                    {
+                        L = L * ((incognita - Abscisas[j]) / (Abscisas[i] - Abscisas[j]));
+                    }
+                    j++;
+                }
+                //Printeamos los L
+                label12.Text = label12.Text + "\n" + "L es: " + L + " en x = " + Abscisas[i] + "P(x)= " + resultado + " + ( " + L + " *  " + Ordenadas[i] + ") ";
+                resultado = resultado + (L * Ordenadas[i]);
+
+                i++;
+            }
+            label13.Text = "Polinomio:\n" + "El resultado es: " + resultado;
+            //Console.ReadKey();
+        }
+
+        static double calculoDeU(double u, int n)
+        {
+            double temp = u;
+            for (int i = 1; i < n; i++)
+                temp = temp * (u - i);
+            return temp;
+        }
+
+        // calcula factorial para la cantidad de puntos
+        static int factorial(int n)
+        {
+            int f = 1;
+            for (int i = 2; i <= n; i++)
+                f *= i;
+            return f;
+        }
+
+        private void dataGridView2_CellContentClick_1(object sender, DataGridViewCellEventArgs fila)
+        {
+            int n = fila.RowIndex;
+            if (n != -1)
+            {
+                Abscisas.Add(Convert.ToDouble(dataGridView1.Rows[n].Cells[0].Value));
+                Ordenadas.Add(Convert.ToDouble(dataGridView1.Rows[n].Cells[1].Value));
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label12.Text = "Pasos:";
+            label13.Text = "Resultado:";
+            if (radioButton4.Checked)
+            {
+                LaGrange(Convert.ToDouble(textBox4.Text));
+            }
+            else if (radioButton5.Checked)
+            {
+                NGP(Convert.ToDouble(textBox4.Text));
+            }
+            else if (radioButton6.Checked)
+            {
+                NGR(Convert.ToDouble(textBox4.Text));
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (textBox3.Text == "(opcional)" || textBox3.Text == "")
+            {
+                int n = dataGridView2.Rows.Add();
+                dataGridView2.Rows[n].Cells[0].Value = Convert.ToDouble(textBox2.Text);
+                dataGridView2.Rows[n].Cells[1].Value = Convert.ToDouble(textBox1.Text);
+                Abscisas.Add(Convert.ToDouble(textBox2.Text));
+                Ordenadas.Add(Convert.ToDouble(textBox1.Text));
+                textBox1.Text = "";
+                textBox2.Text = "";
+            }
+            else
+            {
+                dataGridView2.Rows[Convert.ToInt32(textBox3.Text)].Cells[0].Value = Convert.ToDouble(textBox2.Text);
+                dataGridView2.Rows[Convert.ToInt32(textBox3.Text)].Cells[1].Value = Convert.ToDouble(textBox1.Text);
+                Abscisas[Convert.ToInt32(textBox3.Text)] = Convert.ToDouble(textBox2.Text);
+                Ordenadas[Convert.ToInt32(textBox3.Text)] = Convert.ToDouble(textBox1.Text);
+                textBox1.Text = "";
+                textBox2.Text = "";
+                textBox3.Text = "";
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label12_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void NGP(double k)
+        {
+            // Los puntos ha interpolar
+            int n = Abscisas.Count;
+
+            // y[,] se usa para la tablea
+            // y[,0] para el primer valor
+            double[,] y = new double[n, n];
+            for (int i = 0; i < n; i++)
+            {
+                y[i, 0] = Ordenadas[i];
+            }
+
+            // Calculo de las diferencias respecto a y
+            // tabla
+            label12.Text = label12.Text + '\n';
+            for (int i = 1; i < n; i++)
+            {
+                for (int j = 0; j < n - i; j++)
+                {
+                    y[j, i] = (y[j + 1, i - 1] - y[j, i - 1]);/// (Abscisas[i + j] - Abscisas[j]);
+                    label12.Text = label12.Text + y[j, i] + " ";
+                }
+                label12.Text = label12.Text + '\n';
+            }
+
+            // Muestra la tabla de los puntos y las diferencias con y
+            /*for (int i = 0; i < n; i++)
+            {
+                Console.Write(Abscisas[i] + "\t");
+                for (int j = 0; j < n - i; j++)
+                    Console.Write(y[i, j] + "\t");
+                Console.WriteLine();
+            }*/
+
+            //  lo que queremos interpolar
+            double incognita = k;
+
+            // Usa u  y el factorial
+            double sum = y[0, 0];
+            double u = (incognita - Abscisas[0]) / (Abscisas[1] - Abscisas[0]);
+            for (int i = 1; i < n; i++)
+            {
+                sum = sum + (calculoDeU(u, i) * y[0, i]) /
+                                        factorial(i);
+            }
+
+            label13.Text = label13.Text + "\n El valor de " + incognita + " es " + Math.Round(sum, 6);
+            //Console.ReadKey();
+        }
+
+        public void NGR(double k)
+        {
+            // Los puntos ha interpolar
+            int n = Abscisas.Count;
+            
+            // y[,] se usa para la tablea
+            // y[,0] para el primer valor
+            double[,] y = new double[n, n];
+            for (int i = 0; i < n; i++)
+            {
+                y[i, 0] = Ordenadas[i];
+            }
+
+            // Calculo de las diferencias respecto a y
+            label12.Text = label12.Text + '\n';
+            for (int i = 1; i < n; i++)
+            {
+                for (int j = n - 1; j >= i; j--)
+                {
+                    y[j, i] = (y[j, i - 1] - y[j - 1, i - 1]);/// (Abscisas[i + j] - Abscisas[j]);
+                    label12.Text = label12.Text + y[j, i] + " ";
+                }
+                label12.Text = label12.Text + '\n';
+            }
+
+            // Muestra la tabla de los puntos y las diferencias con y
+
+            // lo que queremos interpolar 
+            double incognita = k;
+
+            // Usa la u y el factorial
+            double sum = y[n - 1, 0];
+            double u = (incognita - Abscisas[n - 1]) / (Abscisas[1] - Abscisas[0]);
+            for (int i = 1; i < n; i++)
+            {
+                sum = sum + (calculoDeU(u, i) * y[n - 1, i]) /
+                                            factorial(i);
+            }
+
+            label13.Text = label13.Text + "\n El valor de " + incognita + " es " + Math.Round(sum, 6);
+            //Console.ReadKey();
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton5_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton6_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label13_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
